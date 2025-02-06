07c9d29532d92f4f820248299b03b7a34b23b30f
Ameen Araf Ali Khan <ameenalikhan28@gmail.com>
2025-02-06 19:06:43 +0530

    generate_parenthesis done

--- Stack/generate_parenthesis.py
+++ Stack/generate_parenthesis.py
@@ -1,2 +1,29 @@
-print("Create repo")
-print("Second commit and push")
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+        
+        def backtrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append('(')
+                backtrack(openN+1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(')')
+                backtrack(openN, closeN + 1)
+                stack.pop()
+        backtrack(0,0)
+        return res
+    
+out = Solution()
+res = out.generateParenthesis(n = 3)
+print("This is the result: ", res)
+print("This is the length of result: ", len(res))
+
+"""The result sequence for different values of n follows the Catalan number sequence."""
+
